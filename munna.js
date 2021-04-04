a6625a1b279cc6407103b653c1ee691710290cf9
Ashikur Rahman <80146103+Ashikur901@users.noreply.github.com>
2021-04-03 21:51:54 -0700

    Add files via upload

---
+++ munna.js
@@ -0,0 +1,90 @@
+    function  clkbtn () {
+    let input = document.getElementById('stdinput').value;
+    //input = parseInt(input);
+    switch (input) {
+        case  'Akame':
+        case  'akame':   
+            document.write('<br> <img width="300px" top="100px" src="img/two.jpg"> <br>  <br> Name: Akame <br><br> Short Backstories: Akame may appear as a cold-blooded assassin to those who do not know her, but the truth behind her character runs deeper. After her parents sold both her and her younger sister, Kurome, to the empire, she was forced to survive, even if it meant killing others.<br>Akame has also been betrayed multiple times by those she trusts, such as a girl named Martha who used an opening to try and cut Akame down. As such, it is not easy to gain her trust.')
+        break;
+
+        case 'Ciel Phantomhive':
+        case 'Ciel ':
+        case 'Phantomhive':
+            document.write('<br> <img width="300px" top="100px"  src="img/six.jpg"> <br><br> Name : Ciel Phantomhive <br><br> Short Backstories: Ciel Phantomhive may still be a child, but that does not make his past any less tragic than other characters. On his tenth birthday, his mother and father, as well as all the servants in his mansion, were murdered by an unidentified assailant .<br>  To top it all off, he was kidnapped by a cult, tortured, and assaulted until he unknowingly helped in summoning a demon with his hatred of the cult members.')
+        break;
+        
+        case 'Ray':
+        case 'ray':
+            document.write('<br> <img  width="300px" top="100px" src="img/two2.jpg" <br> <br> Name : Ray <br> <br> Short Backstories: When fans first see Ray, they think of him as one of the calmest children amongst Emma and Norman. However, Ray is perhaps one of the most tragic characters in the bunch, despite The Promised Neverland being full of tragic characters.<br> It turns out that Ray knew everything all along, and while plotting his escape from Gracefield House since he was a child, he knew Isabella, his own mother, was feeding the children to demons.')
+        break;    
+            
+        case 'Killua Zoldyck':
+        case 'Zoldyck':
+        case 'Killua':
+            document.write('<br><img width="300px" top="100px"  src="img/one.jpg"><br> <br> Name : Killua Zoldyck  <br><br> Short Backstories: Killua was born into a family of assassins, which means his childhood was anything but ordinary. At the age of three, his parents put him in dangerous situations ranging from various forms of torture to seeing if he can dodge lethal attacks.<br>Killua has also killed more people in his time than most adults. As such he had to attack his entire family, even leaving his mother face wrapped in bandages, to escape from the hell that is his life. ')
+        break;    
+            
+        case 'Kyo Sohma ':
+        case 'Sohma':
+            case 'Kyo':   
+               document.write('<br><img width="300px" top="100px"  src="img/three3.jpg"> <br> <br> Name : Kyo Sohma <br><br> Short Backstories: Being born into the Sohma family, one that has its roots in certain people being born as one of the zodiac animals, is already hard enough in Fruits Basket. However, what makes this worse is that Kyo happens to be the cat, which is the most hated zodiac animal.<br> Although Kyo is able to be free during the early events of the series, it turns out those who are born under the cat zodiac are usually left in solitary confinement and are never able to leave. Kyo mother even took her own life before Kyo because she wanted to love her son, but could not because of his hideous true form.')
+        break;   
+            
+        case 'Lelouch Vi Britannia':
+        case 'Lelouch':
+            case 'lelouch':   
+            document.write('<br><img width="300px" top="100px"  src="img/14.jpg"> <br> <br> Name :Lelouch Vi Britannia  <br><br> Short Backstories:Lelouch vi Britannia is proof that even people born of nobility can lead tragic lives. As a child, he witnessed his mother’s assassination, which also crippled his sister, and was later banished to Japan and abandoned by his father. <br>With such tragic circumstances, it is no wonder that he hates his father and the Britannia Empire so much. After receiving the power of Geass, Lelouch adopts the identity of Zero and seeks to bring an end to Britannia’s rule over Japan. Sadly, his desire for revenge overwhelms his desire for justice. In the end, he becomes the very thing he sought to destroy.')
+        break;    
+        
+        case 'The Elric Brothers':
+        case ' Elric Brothers':
+        case 'elric':    
+             document.write('<br><img width="300px" top="100px"  src="img/ten.jpg"> <br> <br> Name : The Elric Brothers  <br><br> Short Backstories:Despite their father’s absence growing up, Edward and Alphonse Elric lived a happy life with their mother Trisha. After she died due to an illness, they were left distraught and Ed came to resent his father. Desperate to bring their mother back, they committed the ultimate taboo of alchemy and performed human transmutation. Needless to say, their attempt resulted in the birth — and subsequent death — of a disfigured monstrosity. <br> What’s worse, the attempt ultimately cost Edward an arm and a leg, while Alphonse lost his entire body. From that point on, Edward and Alphonse went on a long and emotionally painful journey to restore their bodies.')
+        break;
+
+        case 'Itachi Uchiha':
+        case 'itachi':
+            document.write('<br><img width="300px" top="100px"  src="img/twoulb.jpg"> <br> <br> Name :Itachi Uchiha  <br><br> Short Backstories:After seeing the horrors of war as a child, Itachi Uchiha vowed to create a world without violence. Years later, he discovered the hatred and contempt festering within the Uchiha clan. When his clan began planning a coup d’état, he secretly became a double agent for the Hidden Leaf. <br>Despite his best efforts, he was forced to execute his fellow clansmen. In the end, the only survivor was his brother Sasuke, who Itachi convinced to hate him so that Sasuke could avenge their clan. The fact that Itachi successfully carried this burden by himself for so long is nothing short of amazing.')
+        break;
+
+        case 'Hyakkimaru':
+        case 'hyakkimaru':
+            document.write('<br><img width="300px" top="100px"  src="img/seven.jpg"> <br> <br> Name :Hyakkimaru  <br><br> Short Backstories:No matter the version, Dororo’s Hyakkimaru appears destined to suffer from his father ruthlessness. From the moment of his birth, Hyakkimaru father offered him as a sacrifice to several demons in exchange for his land’s prosperity. <br> As a result, the demons rob Hyakkimaru of everything, including his skin, limbs, eyes, voice as well as his spine. His father then orders him to be abandoned. Hyakkimaru is later saved and raised by the doctor Jukai, who gives him artificial prosthetics to help him thrive. Forced to suffer for the sake of another man’s ambition, Hyakkimaru is cursed with a fate worse than death.')
+        break;
+
+        case 'Nico Robin ':
+        case 'nico':
+        case 'robin':    
+            document.write('<br><img width="300px" top="100px"  src="img/one1.jpg"> <br> <br> Name : Nico Robin <br><br> Short Backstories:When it comes to the backstories of One Piece characters, Nico Robin’s is by far one of the most tragic. When she was two years old, her mother left Ohara leaving her vulnerable to both verbal and physical abuse from her aunt, as well as other children. At age eight, the marines attacked her home resulting in the deaths of her mother, her fellow archeologists, as well as her friend Saul. <br>From then on, Robin spent 20 years running from the World Government. During that time, she was forced to fend for herself as everyone she came to trust eventually betrayed her. Until she met the Straw Hats, Robin life was utter hell.')
+        break;
+
+        case 'Crona':
+        case 'crona':
+            document.write('<br><img width="300px" top="100px"  src="img/three.jpg"> <br> <br> Name : Crona  <br><br> Short Backstories: Growing up, Crona suffered constant physical and psychological abuse from both his mother Medusa and his demon weapon Ragnarok. Their harsh treatment eventually drove Crona to the point of insanity. As an agent of madness, Crona does not hesitate to eliminate his opponents and absorbing their souls.<br> In the anime, Crona’s friendship with Maka helps him to break away from his madness. Unfortunately, this does not happen in the manga. He succumbs to madness and performs immoral acts like brutally slaying Medusa and becoming the new Kishin. In the end, one can’t help but feel sorry for Crona.')
+        break;
+
+        case 'Seras Victoria':
+        case 'seras':
+             document.write('<br><img width="300px" top="100px"  src="img/eleven.jpg"> <br> <br> Name : Seras Victoria <br><br> Short Backstories:A former human and police officer, Seras was turned into a vampire by Alucard. For most of the series, Seras past remained a mystery. It wasn’t until the seventh episode of Hellsing Ultimate that details of her past were revealed. The episode shows that her father was an undercover police officer and that the criminals he was investigating tracked him down to their home, where she witnessed them execute her parents. <br> After stabbing one of her attackers, she was shot and left for dead, but not before she was forced to watch as one of the criminals violated her mother corpse. This event continued to haunt her throughout her childhood.')
+        break;
+
+        case 'Lucy':
+        case 'lucy':
+            document.write('<br><img width="300px" top="100px"  src="img/13.jpg"> <br> <br> Name : Lucy <br><br> Short Backstories:As a child, Lucy grew up in an orphanage where she was neglected by her caretakers and bullied by the other children. Her only friend was a puppy that she cared for in secret. After showing the puppy to a fellow orphan, she revealed its existence to Lucy’s bullies. <br> Lucy was then forced to watch as they beat her only friend to death. This traumatic event caused her to lash out and slaughter her tormentors with her vectors. Lucy is then eventually captured and imprisoned in a government facility, furthering her hatred of humans. From a lonely little girl, Lucy interaction with malicious humans turned her into a vicious killing machine.')
+        break;
+
+        case 'Hänsel & Gretel':
+        case 'Hänsel':
+        case 'hänsel':
+        case 'Gretel':
+        case 'gretel':
+             document.write('<br><img width="300px" top="100px"  src="img/eight.jpg"> <br> <br> Name : Hänsel & Gretel <br><br> Short Backstories:Hänsel and Gretel, whose true identities remain unknown, have one of the most horrific pasts in anime. Abandoned at a young age, Hänsel and Gretel were forced to participate in snuff films. They were ordered to murder other children or risk being violated and killed themselves. The constant abuse and horrors they witnessed pushed the twins to embrace the violence, turning them into serial killers. <br>Believing themselves to be “vampires”, the twins thought that they could live forever if they kept taking lives. Sadly, the twins meet their end at the hands of Balalaika after they tortured and massacred her men.')
+        break;
+
+        case 'Guts':
+        case 'guts':
+             document.write('<br><img width="300px" top="100px"  src="img/nine.jpg"> <br> <br> Name : Guts <br><br> Short Backstories: Since birth, Guts has lived his life surrounded by death and violence. After he is found as an infant under his mother hanging corpse by mercenaries, he is taken in and raised as a child soldier. He also suffered severe abuse under his guardian Gambino, who even sold him to one of his soldiers.<br> Guts eventually joins the Band of the Hawk, where he finally develops bonds of friendship and camaraderie. Years later, he is forced to watch as his friend, Griffith, sacrifices his comrades to demons and forces himself onto his lover Casca. Despite surviving the massacre, he is cursed to be hunted by demons every night until he dies.')
+        break;
+
+    }        
+    }
